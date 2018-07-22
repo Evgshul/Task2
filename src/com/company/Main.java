1a9d9866b75766211ba5278f8a231eaf17a34d1a
Evgenij <evgshul@gmail.com>
2018-07-22 15:20:24 +0300

    Not formatted triangle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,29 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter base's length of the triangle :");
         int n = scanner.nextInt();
-        for (int k = 0; k <= n; k = k + 2) {
-            for (int i = n; i <= n + k; i++) {
-                for (int j = 2; j<=2; j--){
-                System.out.printf("%js","*");}
+        if (n % 2 == 0) {
+            for (int j = 0; j <= n; j = j + 2) {
+                for (int i = 0; i < j; i++) {
+                    System.out.print("*");
+                    //System.out.printf("%s", "*");
+                }
+                System.out.println();
             }
-           System.out.println();
+        } else {
+            for (int j = 0; j <= n; j = j + 2) {
+                for (int i = 0; i <= j; i++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
         }
     }
 }
+//System.out.printf("%s\n %s\n %2s\n", "*****", "***", "*");
+
+///System.out.println();
+
+
+
+
+
