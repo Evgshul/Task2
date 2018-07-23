cb873cb560da3e40f990c65b51c75f70200fee62
Evgenij <evgshul@gmail.com>
2018-07-23 14:20:15 +0300

    triangle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,23 +9,23 @@ public class Main {
         System.out.println("Enter base's length of the triangle :");
         int n = scanner.nextInt();
         if (n % 2 == 0) {
-            for (int j = 0; j <= n; j = j + 2) {
-                for (int i = 0; i < j; i++) {
+            for (int j = 1; j <= n; j = j + 2) {
+                for (int i = 0; i <= j; i++) {
                     System.out.print("*");
-                    //System.out.printf("%s", "*");
-                }
+                                    }
                 System.out.println();
             }
         } else {
             for (int j = 0; j <= n; j = j + 2) {
                 for (int i = 0; i <= j; i++) {
-                    System.out.print("*");
+                    System.out.printf("*");
                 }
                 System.out.println();
             }
         }
     }
 }
+
 //System.out.printf("%s\n %s\n %2s\n", "*****", "***", "*");
 
 ///System.out.println();
